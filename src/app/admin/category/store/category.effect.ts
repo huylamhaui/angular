602f3d3901113b10d336e7518ee73da9ed116bf9
lamph11 <lamph11@fpt.com.vn>
2020-11-30 11:35:54 +0700

    change nginx conf

--- src/app/admin/category/store/category.effect.ts
+++ src/app/admin/category/store/category.effect.ts
@@ -3,7 +3,14 @@ import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { of } from 'rxjs';
 import { catchError, map, switchMap } from 'rxjs/operators';
 import { CategoryService } from '../category.service';
-import { createCategoryType, createCategoryTypeFailure, createCategoryTypeSuccess, loadListCategoryType, loadListCategoryTypeFailure, loadListCategoryTypeSuccess } from './category.action';
+import {
+    createCategoryType,
+    createCategoryTypeFailure,
+    createCategoryTypeSuccess,
+    loadListCategoryType,
+    loadListCategoryTypeFailure,
+    loadListCategoryTypeSuccess
+} from './category.action';
 
 @Injectable()
 export class CategoryEffect {
